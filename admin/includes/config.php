494cc76978d8e4a4057e6a242b2f30c6bbb2a5f0
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-02-23 16:28:41 +0100

    Created config file for connection of database with php application

---
+++ admin/includes/config.php
@@ -0,0 +1,17 @@
+<?php
+//Database connection constants
+
+define('DB_HOST','localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'gallery_db');
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if($connection) {
+    echo "true";
+}
+
+
+
+?>
