21a2309a39f14ac25877fe35d6c473a31a67027f
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-02-26 14:12:58 +0100

    Added session value function in session.php

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -13,6 +13,21 @@ function __construct()
     session_start();
 }
 
+private function check_the_login(){
+    //Check if there is user id on session
+    if(isset($_SESSION['user_id'])){
+        //Give the session that value
+        $this->user_id = $_SESSION['user_id'];
+        $this->signed_in = true;
+    } else {
+        //Unset the values and make sign in false
+        unset($this->user_id);
+        $this->signed_in = false;
+
+    }
+
+}
+
 
 }
 
