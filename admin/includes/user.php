20865f76939b4670b23c79411271bc6c697649e6
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-01 22:08:22 +0100

    Created delete method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -103,10 +103,20 @@ class User {
         $database->query($sql);
         //Pre built function of mysqli
         return (mysqli_affected_rows($database->connection) == 1) ? true : false;
+    }
 
 
+    public function delete(){
+        global $database;
+        //Deleting from the database
+        $sql = "DELETE fROM users ";
+        $sql .= "username= '" . $database->escape_string($this->username) . "', ";
+        $sql .= "password= '" . $database->escape_string($this->password) . "', ";
+        $sql .= "first_name= '" . $database->escape_string($this->first_name) . "', ";
+        $sql .= "last_name= '" . $database->escape_string($this->last_name) . "' ";
+        $sql .= " WHERE id= " . $database->escape_string($this->id);
 
-
+        $database->query($sql);
 
     }
 
