97ab3e529efbcbdcee6d7cec7fd49e212f3323de
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-04 14:54:50 +0100

    Added photo table properties in photo class

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -1,8 +1,16 @@
 <?php
 
 class Photo extends Db_object {
+    //Properties for photos table
+    protected static $db_table = "photos";
+    protected static $db_table_fields = array('photo_id', 'title', 'description', 'filename', 'type', 'size');
+    public $photo_id;
+    public $title;
+    public $description;
+    public $filename;
+    public $type;
+    public $size;
 
-    
 }
 
 
