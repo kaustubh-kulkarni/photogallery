d8eb15076d4803cc6d6a878ed40c3537d4d7277e
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-04 18:40:10 +0100

    Added functinality for upload table

--- admin/photos.php
+++ admin/photos.php
@@ -1,4 +1,12 @@
 <?php include("includes/header.php"); ?>
+<?php //If user is not signed in 
+if(!$session -> is_signed_in()){redirect("login.php");}?>
+
+<?php
+
+$photos = Photo::find_all();
+
+?>
 
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -28,13 +36,17 @@
                             </tr>
                         </thead>
                         <tbody>
+
+                            <?php foreach ($photos as $photo) : ?>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td><img src="http://placehold.it/62x62" alt=""></td>
+                                <td><?php echo $photo->photo_id;   ?></td>
+                                <td><?php echo $photo->filename;   ?></td>
+                                <td><?php echo $photo->title;   ?></td>
+                                <td><?php echo $photo->size;   ?></td>
                             </tr>
+                            <?php endforeach;   ?>
+
                         </tbody>
                     </table>
                 </div>
