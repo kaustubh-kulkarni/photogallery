7ee075c03e8061e36a075f9a2b7c719963826930
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-04 15:11:01 +0100

    Created error array with values in photo.php

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -10,6 +10,21 @@ class Photo extends Db_object {
     public $filename;
     public $type;
     public $size;
+    //Temporary path for images to move 
+    public $tmp_path;
+    public $upload_directory = "images";
+    public $custom_errors = array();
+    public $upload_errors_array = array (
+        UPLOAD_ERR_OK => "There is no error",
+        UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload max filesize limit",
+        UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the max filesize limit",
+        UPLOAD_ERR_PARTIAL => "PARTIAL ERROR",
+        UPLOAD_ERR_PARTIAL => "The uploaded file was only partially uploaded",
+        UPLOAD_ERR_NO_FILE => "No file was uploaded",
+        UPLOAD_ERR_NO_TMP_DIR => "Missing Temporary folder",
+        UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk",
+        UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload"
+        );
 
 }
 
