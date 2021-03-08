476bef3a028f3989545e47568a15fa1db6cb4656
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-08 16:03:56 +0100

    Created functioning delete_photo.php

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -4,9 +4,17 @@ if(!$session -> is_signed_in()){redirect("login.php");}?>
 
 <?php
 
+//If not photo id then redirect
+if(empty($_GET['photo_id'])){
+    redirect("photos.php");
+}
+//Instanciate with Photo class
+$photo = Photo::find_by_id($_GET['photo_id']);
 
-echo "page created";
-
-
+if($photo){
+    $photo->delete_photo();
+} else {
+    redirect("photos.php");
+}
 
 ?>
