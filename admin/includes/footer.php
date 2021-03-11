ffc1ff55249eee88ac2d685a8b78a3cd179384e9
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-11 10:19:28 +0100

    Added tiny MCE to application as a text editor

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,12 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <!-- Tiny MCE WYSIWYG editor -->
+    <script src="http://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
+    <!-- Custom JS -->
+    <script src="js/scripts.js"></script>
+
 </body>
 
 </html>
