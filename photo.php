87fe262f63a9d2c38d24eeb4baa3db7c8318c2a2
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-13 23:26:58 +0100

    Updated photo frontend

--- photo.php
+++ photo.php
@@ -1,5 +1,14 @@
 <?php
 
+require_once("admin/includes/init.php");
+
+if(empty($_GET['id'])){
+    redirect("index.php");
+}
+$photo = Photo::find_by_id($_GET['id']);
+
+echo $photo->title;
+
 if(isset($_POST['submit'])){
     echo "It works";
 }
