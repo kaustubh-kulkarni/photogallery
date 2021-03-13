aee39c3f3155a2d9092bc6d7e8d3d8b904de60a5
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-13 22:51:34 +0100

    Created create comment method in comment class

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -1,70 +1,25 @@
 <?php
 //User class
-class User extends Db_object {
-    protected static $db_table = "users";
-    protected static $db_table_fields = array('username', 'password', 'first_name', 'last_name', 'user_image');
+class Comment extends Db_object {
+    protected static $db_table = "comments";
+    protected static $db_table_fields = array('id', 'photo_id', 'author', 'body');
     public $id;
-    public $username;
-    public $password;
-    public $first_name;
-    public $last_name;
-    public $user_image;
-    public $upload_directory = "images";
-    public $image_placeholder = "http://placehold.it/400x400&text=image";
+    public $photo_id;
+    public $author;
+    public $body;
     
+    //Class instantiation and methods
+    public static function create_comment($photo_id, $author="John Doe", $body=""){
+        if(!empty($photo_id) && !empty($author) && !empty($body)){
+            $comment = new Comment();
+            //Assign values
+            $comment->photo_id = (int)$photo_id;
+            $comment->author = $author;
+            $comment->body = $body;
 
-    public function upload_photo() {
-        //Error checking
-            if(!empty($this->errors)){
-                return false;
-            }
-            if(empty($this->user_image) || empty($this->tmp_path)) {
-                $this->errors[] = "The file was not available";
-                return false;
-            }
-            $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->user_image;
-
-            if(file_exists($target_path)) {
-                $this->errors[] = "The file {$this->user_image} already exists";
-                return false;
-            }
-
-            //Predefined function to move the uploaded file
-            if(move_uploaded_file($this->tmp_path, $target_path)) {
-                    unset($this->tmp_path);
-                    return true;
-            } else {
-                $this->errors[] = "The file directory does not have permission";
-                return false;
-            }
+            return $comment;
+        } else {
+            return false;
+        }
     }
-
-   
-    
-    public static function verify_user($username, $password){
-        global $database;
-
-        $username = $database->escape_string($username);
-        $password = $database->escape_string($password);
-        //Getting username and pass from DB
-        $sql = "SELECT * FROM " .self::$db_table. " WHERE username = '{$username}' AND password ='{$password}' LIMIT 1";
-
-        $the_result_array = self::find_by_query($sql);
-        return !empty($the_result_array) ? array_shift($the_result_array) : false;
-
-    }
-
-    public function image_path_and_placeholder(){
-        return empty($this->user_image) ? $this->image_placeholder : $this->upload_directory.DS.$this->user_image;
-    }
-
-    
-   
-
-
-   
-
 }
-
-
-?>
