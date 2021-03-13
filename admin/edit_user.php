adf71763b3dad6391efe980163a8c02a7f68c97b
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-13 12:09:05 +0100

    Created edit user page where user can edit the information

---
+++ admin/edit_user.php
@@ -0,0 +1,85 @@
+<?php include("includes/header.php"); ?>
+<?php //If user is not signed in 
+if (!$session->is_signed_in()) {
+    redirect("login.php");
+} ?>
+<?php
+//If dont have an ID redirect
+if (empty($_GET['id'])) {
+    redirect("photos.php");
+}
+
+$user = User::find_by_id($_GET['id']);
+if (isset($_POST['update'])) {
+
+    if ($user) {
+        $user->username = $_POST['username'];
+        $user->first_name = $_POST['first_name'];
+        $user->last_name = $_POST['last_name'];
+        $user->password = $_POST['password'];
+        $user->set_file($_FILES['user_image']);
+        $user->save_user_and_image();
+    }
+}
+
+
+
+
+
+
+?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <?php include("includes/top_nav.php") ?>
+    <?php include("includes/side_nav.php") ?>
+</nav>
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Users
+                    <small>Subheading</small>
+                </h1>
+
+                <!-- Form control -->
+                <form action="" method="POST" enctype="multipart/form-data">
+                    <div class="col-md-6 col-md-offset-3">
+                        <div class="form-group">
+                            <input type="file" name="user_image">
+                        </div>
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" name="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" name="first_name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input type="text" name="last_name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="update" class="btn btn-primary pull-right">
+                        </div>
+                    </div>
+                </form>
+
+
+
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div>
+
+<?php include("includes/footer.php"); ?>
