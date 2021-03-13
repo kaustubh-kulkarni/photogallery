0eb4b52dc7de3aa813ebbb6a3debc241f3a0e3ad
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-13 14:26:18 +0100

    Updated user_edit.php

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -69,7 +69,7 @@ if (isset($_POST['update'])) {
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input type="password" name="password" class="form-control">
+                            <input type="password" name="password" class="form-control" value="<?php echo $user->password;  ?>">
                         </div>
                         <div class="form-group">
                             <input type="submit" name="update" class="btn btn-primary pull-right" value="Update">
