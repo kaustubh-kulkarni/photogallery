ee63f77ba7a6810707a31f96d4295002a143bbb8
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-13 23:44:38 +0100

    Updated few methods

--- photo.php
+++ photo.php
@@ -27,7 +27,9 @@ if(isset($_POST['submit'])){
     $body = "";
 }
 
-Comment::find_the_comments($photo->id);
+$comments = Comment::find_the_comments($photo->id);
+
+
 
 
 
@@ -160,19 +162,22 @@ Comment::find_the_comments($photo->id);
                 <hr>
 
                 <!-- Posted Comments -->
-
+                <?php
+                foreach ($comments as $comment):
+                ?>
                 <!-- Comment -->
                 <div class="media">
                     <a class="pull-left" href="#">
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
                     <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
+                        <h4 class="media-heading"><?php echo $comment->author; ?>
                             <small>August 25, 2014 at 9:30 PM</small>
                         </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        <?php echo $comment->body; ?>
                     </div>
                 </div>
+                <?php endforeach ?>
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
