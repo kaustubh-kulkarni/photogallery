f4c4e7aac78dc9d47512542db722b83360da1ada
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-13 13:15:48 +0100

    Updated user information in user_edit.php

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -44,31 +44,35 @@ if (isset($_POST['update'])) {
                     Users
                     <small>Subheading</small>
                 </h1>
-
+                <div class="col-md-6">
+                    <img class="img-responsive" src="<?php echo $user->image_path_and_placeholder();   ?>" alt="">
+                
+                </div>
                 <!-- Form control -->
                 <form action="" method="POST" enctype="multipart/form-data">
-                    <div class="col-md-6 col-md-offset-3">
+
+                    <div class="col-md-6">
                         <div class="form-group">
                             <input type="file" name="user_image">
                         </div>
                         <div class="form-group">
                             <label for="username">Username</label>
-                            <input type="text" name="username" class="form-control">
+                            <input type="text" name="username" class="form-control" value="<?php echo $user->username;  ?>">
                         </div>
                         <div class="form-group">
                             <label for="first_name">First Name</label>
-                            <input type="text" name="first_name" class="form-control">
+                            <input type="text" name="first_name" class="form-control" value="<?php echo $user->first_name;  ?>">
                         </div>
                         <div class="form-group">
                             <label for="last_name">Last Name</label>
-                            <input type="text" name="last_name" class="form-control">
+                            <input type="text" name="last_name" class="form-control" value="<?php echo $user->last_name;  ?>">
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
                             <input type="password" name="password" class="form-control">
                         </div>
                         <div class="form-group">
-                            <input type="submit" name="update" class="btn btn-primary pull-right">
+                            <input type="submit" name="update" class="btn btn-primary pull-right" value="Update">
                         </div>
                     </div>
                 </form>
