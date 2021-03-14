e2118c4c4e0dbd6bc2c93fae2e6bb25f0c7e9ffb
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-14 16:39:30 +0100

    Updated photos.php with another column

--- admin/photos.php
+++ admin/photos.php
@@ -33,6 +33,7 @@ $photos = Photo::find_all();
                                 <th>File Name</th>
                                 <th>Title</th>
                                 <th>Size</th>
+                                <th>Comments</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -52,6 +53,11 @@ $photos = Photo::find_all();
                                 <td><?php echo $photo->filename;   ?></td>
                                 <td><?php echo $photo->title;   ?></td>
                                 <td><?php echo $photo->size;   ?></td>
+                                <td><?php
+                                    $comments = Comment::find_the_comments($photo->id);
+                                    echo count($comments);
+                                ?>
+                                </td>
                             </tr>
                             <?php endforeach;   ?>
 
