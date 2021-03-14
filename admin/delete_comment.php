99910ce9caaed4c6c22a99fc3d27d055658b274d
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-14 15:53:25 +0100

    Added delete comment functionality

---
+++ admin/delete_comment.php
@@ -0,0 +1,21 @@
+<?php include("includes/header.php"); ?>
+<?php //If user is not signed in 
+if(!$session -> is_signed_in()){redirect("login.php");}?>
+
+<?php
+
+//If not photo id then redirect
+if(empty($_GET['id'])){
+    redirect("comments.php");
+}
+//Instanciate with Photo class
+$comment = Comment::find_by_id($_GET['id']);
+
+if($comment){
+    $comment->delete();
+    redirect("comments.php");
+} else {
+    redirect("comments.php");
+}
+
+?>
