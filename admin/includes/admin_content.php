0f459649f4b40574ef4473916201cb546ec3619f
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-14 22:12:17 +0100

    Updated admin content

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -4,7 +4,7 @@
         <div class="col-lg-12">
             <h1 class="page-header">
                 Blank Page
-                <small>Subheading</small>
+                <small>Dashboard</small>
             </h1>
 
             <div class="row">
