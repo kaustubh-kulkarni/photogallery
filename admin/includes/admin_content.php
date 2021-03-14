013d016f46f156e932048baf8bdf11721fe0a27b
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-14 21:57:43 +0100

    Updated dashboard with photo, user and comment count

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -63,8 +63,8 @@
                                     <i class="fa fa-user fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <div class="huge">8
-
+                                    <div class="huge">
+                                        <?php echo User::count_all(); ?>
                                     </div>
 
                                     <div>Users</div>
@@ -89,7 +89,7 @@
                                     <i class="fa fa-support fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <div class="huge">8</div>
+                                    <div class="huge"><?php echo Comment::count_all();  ?></div>
                                     <div>Comments</div>
                                 </div>
                             </div>
