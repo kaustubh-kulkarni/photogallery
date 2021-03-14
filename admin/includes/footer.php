0eb835ab564aadde9f4b88e1e567323233fc49f3
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-14 22:11:48 +0100

    Update google chart with dynamic values

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -21,15 +21,19 @@
 
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
+          ['Views', <?php echo $session->count; ?>],
+          ['Comments', <?php echo Comment::count_all(); ?>],
+          ['Users',  <?php echo User::count_all(); ?>],
+          ['Photos', <?php echo Photo::count_all(); ?>]
+          
+          
         ]);
 
         var options = {
-          title: 'My Daily Activities'
+          legend: 'none',
+          pieSliceText: 'label',
+          title: 'Dashboard',
+          backgroundColor: 'transparent'
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
