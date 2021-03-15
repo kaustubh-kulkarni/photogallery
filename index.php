6479ddb550e1dc449575e1d31f83ac4ff170233c
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-15 11:34:40 +0100

    Updated and created custom stylesheet for frontend

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <?php  foreach ($photos as $photo): ?>
                 <div class="col-xs-6 col-md-3">
                     <a class="thumbnail" href="">
-                        <img src="admin/<?php echo $photo->picture_path(); ?>" alt="">
+                        <img class="homepage_photo" src="admin/<?php echo $photo->picture_path(); ?>" alt="">
                     </a>
                 </div>
         <?php endforeach; ?>
