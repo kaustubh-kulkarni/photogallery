5634b706c6c38212def397e50921af077690b193
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-15 11:23:51 +0100

    Updated index.php on our main frontend

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php include("includes/header.php"); ?>
+
+<?php   
+    //Find all photos
+    $photos = Photo::find_all();
+
+
+?>
+
 <div class="row">
     <!-- Blog Entries Column -->
-    <div class="col-md-8">
-    </div>
-    <!-- Blog Sidebar Widgets Column -->
-    <div class="col-md-4">
-        <?php include("includes/sidebar.php"); ?>
+    <div class="col-md-12">
+        <?php  foreach ($photos as $photo): ?>
+            <div class="thumbnail row">
+                <div class="col-xs-6 col-md-3">
+                    <a class="thumbnail" href="">
+                        <img src="" alt="">
+                    </a>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
     <!-- /.row -->
 <?php include("includes/footer.php"); ?>
