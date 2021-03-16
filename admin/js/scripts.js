b942bef9f66ea333f567b867c574359580d7f05e
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-16 10:18:14 +0100

    Updated functionality of modal using JQ

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+    $(".modal_thumbnails").click(function() {
+        $("#set_user_image").prop('disabled', false);
+    });
+
     tinymce.init({
         selector: 'textarea'
     });
