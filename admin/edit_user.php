0fd1ebd017f9f9d54cc16f396dcf9725df4a1f52
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-16 13:08:37 +0100

    Created JS method to get user id from href

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -79,7 +79,7 @@ if (isset($_POST['update'])) {
                             <input type="password" name="password" class="form-control" value="<?php echo $user->password;  ?>">
                         </div>
                         <div class="form-group">
-                            <a class="btn btn-danger" href="delete_user.php?id=<?php echo $user->id ?>">Delete</a>
+                            <a class="btn btn-danger" id="user-id" href="delete_user.php?id=<?php echo $user->id ?>">Delete</a>
                             <input type="submit" name="update" class="btn btn-primary pull-right" value="Update">
                         </div>
                     </div>
