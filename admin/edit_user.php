119c8379452ddf978454ae4ae1cb7bbf07919e53
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-17 22:38:16 +0100

    Updated message notifications CRUD

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -23,12 +23,12 @@ if (isset($_POST['update'])) {
         if(empty($_FILES['user_image'])) {
             $user->save();
             redirect("users.php");
-            $session->message("The user has been updated");
+            $session->message("{$user->username} has been updated");
         } else {
             $user->set_file($_FILES['user_image']);
             $user->upload_photo();
             $user->save();
-            $session->message("The user has been updated");
+            $session->message("{$user->username} has been updated");
 
             redirect("users.php");
         } 
